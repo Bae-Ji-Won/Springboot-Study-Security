94b41584c4f1bb04e2f1556740ecd7202c06373b
Bae Ji Won <qowhxk@naver.com>
2022-12-06 23:05:54 +0900

    [11/29] ErrorCode (로그인 했을때 출력되는 에러 종류 추가)

--- src/main/java/com/springboot/security/Exception/ErrorCode.java
+++ src/main/java/com/springboot/security/Exception/ErrorCode.java
@@ -13,7 +13,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     // enum을 통해 미리 값을 설정한다.
     // DUPLICATED_USER_NAME의 이름을 가진 에러에 (에러상태 Conflict(409) 출력, 에러메세지)의 값을 넣은 구조만 받는다.
-    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated.");
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated."),  // 중복검사할때 에러
+    NOT_FOUND(HttpStatus.NOT_FOUND,"Not Found"),        // 로그인할때 에러
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST,"");
 
     private HttpStatus status;
     private String message;
