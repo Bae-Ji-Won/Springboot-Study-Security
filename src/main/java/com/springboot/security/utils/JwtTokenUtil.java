a43a226689deafb3c30486e24669655f0b01ba58
Bae Ji Won <qowhxk@naver.com>
2022-12-07 00:33:33 +0900

    [11/29] JwtToken Util 생성(토큰에 담을 정보들 입력 / 1. 유저이름, 2. 토큰 정보(유지시간 등))

---
+++ src/main/java/com/springboot/security/utils/JwtTokenUtil.java
@@ -0,0 +1,22 @@
+package com.springboot.security.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtTokenUtil {
+    public static String createToken(String userName, String key, long expireTimeMs) {
+        Claims claims = Jwts.claims(); // 일종의 map
+        claims.put("userName", userName);
+
+        return Jwts.builder()       // 토큰 생성
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))      // 시작 시간 : 현재 시간기준으로 만들어짐
+                .setExpiration(new Date(System.currentTimeMillis() + expireTimeMs))     // 끝나는 시간 : 지금 시간 + 정해둔 시간
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact()
+                ;
+    }
+}
