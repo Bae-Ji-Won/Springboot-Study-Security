6c0c8a62cea9a1265749ab26fc43641ddcd43414
Bae Ji Won <qowhxk@naver.com>
2022-12-09 23:05:35 +0900

    [11/28] HospitalReviewAppException Exception 생성(세부적인 에러통제를 위해 내가 원하는 값으로 에러종류 만들기)

--- src/main/java/com/springboot/security/Exception/HospitalReviewAppException.java
+++ src/main/java/com/springboot/security/Exception/HospitalReviewAppException.java
@@ -10,6 +10,8 @@ public class HospitalReviewAppException extends RuntimeException{
     private ErrorCode errorCode;    //  Service로부터 생성자를 통해 ErrorCode.DUPLICATED_USER_NAME 저장됨
     private String message;         // Service로부터 생성자를 통해 String.format("Username :"+request.getUserName()) 저장됨
 
+    // 원래 RuntimeException이 String으로 에러내용을 반환하기 때문에 상속받은 현재 HospitalReviewAppException도 오버라이딩하여 원하는 에러내용을 반환한다.
+    // 하지만 ExceptionManager로 리턴해주는 값이 우선순위가 더 높기 때문에 ExceptionManager의 값이 반환된다.
     @Override
     public String toString() {
         if(message == null) {
